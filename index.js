6aeb995b163e48f8ab310d498b170aa66367d74a
PJC-1 <jumpzine@gmail.com>
2018-06-10 20:43:56 -0700

    required cluster module and added code to check master mode

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration) {
-  const start = Date.now();
-  while(Date.now() - start < duration) {}
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* but
+  // in child/slave mode
+  cluster.fork();
+} else {
+  // Im a child/slave, Im going to act like a server
+  // and do nothing else
+  const express = require('express');
+  const app = express();
+
+  function doWork(duration) {
+    const start = Date.now();
+    while(Date.now() - start < duration) {}
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there');
+  });
+
+  app.listen(3000, () => {
+    console.log('Your listening to the smooth sound of port 3000...');
+  });
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send('Hi there');
-});
-
-app.listen(3000, () => {
-  console.log('Your listening to the smooth sound of port 3000...');
-});
